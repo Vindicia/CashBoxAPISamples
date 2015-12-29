1af5be8b0724141bcdbf1a45427a3a44353a73a7
rmminnh <rmorrison@vindicia.com>
2015-12-29 14:07:26 -0500

    Create index.php

---
+++ UC-038_update_child_paymentMethod/php/16.0/index.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+
+<h1>Payment Method Update example</h1>
+
+<p>
+Please enter the merchantPaymentMethodID of the Payment Method you would like to update:
+
+<form action="update.php" method="post">
+              
+
+			<form action="update.html" method="post" name="f" class="signin" id="f">
+                  <div style="margin: 0pt; padding: 0pt;">
+                    
+                    
+                    Payment Method ID <input type="text" id="merchantPmId" name="merchantPmId" title="PaymentMethod ID" />
+                       <input id="submit" name="commit" value="Submit" type="submit" />
+                </form>
+
+ </form>
+
+
+</body>
+</html>
